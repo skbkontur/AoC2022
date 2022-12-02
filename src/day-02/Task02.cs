203e5a2c49e8e3aabff14190160e2a73e56befa8
tolltech <alexandrovpe@gmail.com>
2022-12-02 15:22:47 +0400

    day 02

---
+++ src/day-02/Task02.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2021
+{
+    [TestFixture]
+    public class Task02
+    {
+        [Test]
+        [TestCase(
+            @"A Y
+B X
+C Z",
+            15)]
+        [TestCase(
+            @"Task02.txt"
+            ,
+            10816)]
+        public void Task(string input, int expected)
+        {
+            input = File.Exists(input) ? File.ReadAllText(input) : input;
+
+            var rounds = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => (Elf: x[0] - 'A', Me: x[2] - 'X'))
+                .ToArray();
+
+            var score = 0;
+            foreach (var round in rounds)
+            {
+                var (elf, me) = round;
+                
+                score += me + 1;
+
+                score += (me - elf + 1 + 3) % 3 * 3;                
+            }
+
+            score.Should().Be(expected);
+        }
+    }
+}
---
+++ src/day-02/Task02_2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2021
+{
+    [TestFixture]
+    public class Task02_2
+    {
+        [Test]
+        [TestCase(
+            @"A Y
+B X
+C Z",
+            12)]
+        [TestCase(
+            @"Task02.txt"
+            ,
+            11657)]
+        public void Task(string input, int expected)
+        {
+            input = File.Exists(input) ? File.ReadAllText(input) : input;
+
+            var rounds = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => (Elf: x[0] - 'A', Me: x[2] - 'X'))
+                .ToArray();
+
+            var score = 0;
+            foreach (var round in rounds)
+            {
+                var (elf, me) = round;
+
+                score += me * 3;
+
+                score += (elf + (me - 1) + 3) % 3;
+                score += 1;
+            }
+
+            score.Should().Be(expected);
+        }
+    }
+}
